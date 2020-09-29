f81bb1df11e070593f1520764071b8e7b838bc10
JunHao Chen <jaden.chen163@gmail.com>
2020-09-29 15:33:45 -0400

    Added weather details

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
-import Home from './containers/Home';
+import Home from "./containers/Home";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Router path='/'>
+        <Router path="/">
           <Home />
         </Router>
       </Switch>
     </Router>
-  )
+  );
 }
 
 export default App;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,27 @@
 import React from "react";
 
+import Header from "../components/Header";
+
 const weather_key = `3fa26f65abf375ee7374a7f1178b63ba`;
 
-function Home(){
-    return (
-    <div>
-        <h1>Weather App</h1>
+function Home() {
+  return (
+    <div className="Home">
+      <Header />
+      <main>
+        <h2>Weather in Seoul</h2>
+        <div className="WeatherInfo">
+          <p>Weather Type: Cloudy</p>
+          <p>Current Temperature: 100 degrees</p>
+          <p>High Temperature: 100 degrees</p>
+          <p>Low Temperature: 80 degrees</p>
+          <p>Cloudiness: 100</p>
+          <p>Humidity 35%</p>
+          <p>Wind Speed: 3km/h</p>
+        </div>
+      </main>
     </div>
-    )
+  );
 }
 
 export default Home;
