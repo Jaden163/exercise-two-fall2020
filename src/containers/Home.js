cfbed5713362519163cf4b3a80ff9dd61714d298
JunHao Chen <jaden.chen163@gmail.com>
2020-10-06 14:29:13 -0400

    City query completed

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,23 +1,93 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 import Header from "../components/Header";
-
 const weather_key = `3fa26f65abf375ee7374a7f1178b63ba`;
 
 function Home() {
+  const history = useHistory();
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("Seoul");
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weather_key}`
+      )
+      .then(function (response) {
+        // get request to URL
+        setWeatherData(response.data);
+      })
+      .catch(function (error) {
+        // error handle
+        console.log(error);
+      });
+  }, [city]);
+
+  useEffect(() => {
+    const searchParams = history.location.search;
+    const urlParams = new URLSearchParams(searchParams);
+    const city = urlParams.get("city");
+    if (city) {
+      setCity(city);
+    }
+  }, [history]);
+
+  // derive value and store it as variable
+  const {
+    // is a object
+    cloudiness,
+    currentTemp,
+    highTemp,
+    humidity,
+    lowTemp,
+    weatherType,
+    windSpeed,
+  } = useMemo(() => {
+    // local vars
+    let cloudiness = "";
+    let currentTemp = "";
+    let highTemp = "";
+    let humidity = "";
+    let lowTemp = "";
+    let weatherType = "";
+    let windSpeed = "";
+
+    if (weatherData) {
+      cloudiness = `${weatherData.clouds.all}%`;
+      currentTemp = `${weatherData.main.temp}`;
+      highTemp = `${weatherData.main.temp_max}`;
+      humidity = `${weatherData.main.humidity}%`;
+      lowTemp = `${weatherData.main.temp_min}`;
+      weatherType = `${weatherData.weather[0].description}`;
+      windSpeed = `${weatherData.wind.speed} km/h`;
+    }
+
+    return {
+      cloudiness,
+      currentTemp,
+      highTemp,
+      humidity,
+      lowTemp,
+      weatherType,
+      windSpeed,
+    };
+  }, [weatherData]);
+
   return (
-    <div className="Home">
+    <div>
       <Header />
-      <main>
-        <h2>Weather in Seoul</h2>
+      <main className="Home">
+        <h2>Weather in {city}</h2>
         <div className="WeatherInfo">
-          <p>Weather Type: Cloudy</p>
-          <p>Current Temperature: 100 degrees</p>
-          <p>High Temperature: 100 degrees</p>
-          <p>Low Temperature: 80 degrees</p>
-          <p>Cloudiness: 100</p>
-          <p>Humidity 35%</p>
-          <p>Wind Speed: 3km/h</p>
+          <p>Weather Type: {weatherType} </p>
+          <p>Current Temperature: {currentTemp} </p>
+          <p>High Temperature: {highTemp} </p>
+          <p>Low Temperature: {lowTemp} </p>
+          <p>Cloudiness: {cloudiness} </p>
+          <p>Humidity {humidity} </p>
+          <p>Wind Speed: {windSpeed} </p>
         </div>
       </main>
     </div>
